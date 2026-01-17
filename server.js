65d4f7aca0903621399765a0a749e48fb6a9c3d9
Souravjr11 <152426682+Souravjr11@users.noreply.github.com>
2026-01-17 18:06:23 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   }));
-  app.options("*", cors());
+  app.options(/.*/, cors());
   
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
